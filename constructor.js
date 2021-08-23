c2fd4ead60e97b8e3631cb47c61f4ebf837b3d51
Pendela Jaya Bala Kala Manoj <pendela.manoj@hotmail.com>
2021-08-23 19:37:28 +0530

    constructor updated

--- constructor.js
+++ constructor.js
@@ -2,16 +2,19 @@
 function Person(person_name, person_age, person_gender) {
 
 	// assigning  parameter values to the calling object
-	this.name = person_name,
+		this.name = person_name,
 		this.age = person_age,
 		this.gender = person_gender,
 
 		this.greet = function() {
 			return ('Hi' + ' ' + this.name);
 		}
+
 }
 
 let person1 = new Person("jay", 20, "male")
 let person2 = new Person("Manoj", 22, "female")
-console.log(person1.name);
+
+console.log(person1.age);
 console.log(person2.name);
+console.log(person1.greet());
